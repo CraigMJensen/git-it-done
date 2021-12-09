19e7cddc48173fb3c3f070b7c84bdce554c319c8
CraigJensen <cmjensen82@gmail.com>
2021-12-08 20:44:04 -0600

    display 30 issues and have message with link to more issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector('#issues-container');
+var limitWarningEl = document.querySelector('#limit-warning');
 
 var getRepoIssues = function (repo) {
   var apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc';
@@ -7,8 +8,11 @@ var getRepoIssues = function (repo) {
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        // passes response data to the dom function
         displayIssues(data);
+        if (response.headers.get('Link')) {
+          displayWarning(repo);
+        }
+        // passes response data to the dom function
       });
     } else {
       alert('There was a problem with your response!');
@@ -47,6 +51,15 @@ var displayIssues = function (issues) {
     issueEl.appendChild(typeEl);
     issueContainerEl.appendChild(issueEl);
   }
+};
+
+var displayWarning = function (repo) {
+  limitWarningEl.textContent = 'To see more than 30 issues, visit ';
+  var linkEl = document.createElement('a');
+  linkEl.textContent = 'See More Issues on Github.com';
+  linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues');
+  linkEl.setAttribute('target', '_blank');
+  limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues('facebook/react');
