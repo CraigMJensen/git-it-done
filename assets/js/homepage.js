7ddcb8b7fba3b664ffe40784661f09b3b5de50d9
CraigJensen <cmjensen82@gmail.com>
2021-12-09 17:53:17 -0600

    can search by languages as well as username

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector('#language-buttons');
 
 var getUserRepos = function (user) {
   var apiUrl = 'https://api.github.com/users/' + user + '/repos';
@@ -21,6 +22,22 @@ var getUserRepos = function (user) {
     });
 };
 
+var getFeaturedRepos = function (language) {
+  var apiUrl =
+    'https://api.github.com/search/repositories?q=' +
+    language +
+    '+is:featured&sort=help-wanted-issues';
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: Github User Not Found');
+    }
+  });
+};
+
 var formSubmitHandler = function (event) {
   event.preventDefault();
   var username = nameInputEl.value.trim();
@@ -30,6 +47,14 @@ var formSubmitHandler = function (event) {
     nameInputEl.value = '';
   } else {
     alert('Please enter a Github username');
+  }
+};
+
+var buttonClickHandler = function (event) {
+  var language = event.target.getAttribute('data-language');
+  if (language) {
+    getFeaturedRepos(language);
+    repoContainerEl.textContent = '';
   }
 };
 
@@ -67,3 +92,4 @@ var displayRepos = function (repos, searchTerm) {
 };
 
 userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener('click', buttonClickHandler);
